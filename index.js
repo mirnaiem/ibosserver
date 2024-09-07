7663046310c52cd6d7bb092e849ae66fdab06f7c
mirnaiem <mirnaiem8teen@gmail.com>
2024-09-07 23:05:29 +0600

    vercel added

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
     const database=client.db('iboss');
 const productCollection=database.collection('productCollection');
 const cartCollection=database.collection('cartCollection');
@@ -64,11 +63,9 @@ app.get('/carts/:email',async(req,res)=>{
 });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
